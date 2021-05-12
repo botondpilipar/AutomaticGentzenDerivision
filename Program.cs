86d3ec017aa9ae0f03e14be04deea31ad85e5f52
Botond Pilipár <botondpilipar@github.com>
2021-05-12 21:06:45 +0200

    Create false statement in main

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace gentzen_calc
         static void Main(string[] args)
         {
             //  |= (!b V c ) => ((a ^ b ) => c)
-            var rightSide = 
+            var trueRightSide = 
                 new Implication(
                     new Or(
                         new Negate(
@@ -22,10 +22,27 @@ namespace gentzen_calc
                             new Atomic("b")),
                         new Atomic("c")));
 
-            var proofSituation = new ProofSituation(rightSide);
-            ReductionTree tree = new ReductionTree(proofSituation);
-            bool truth = tree.IsAxiome();
-            Console.WriteLine(tree.ToString());
+            var firstProofSituation = new ProofSituation(trueRightSide);
+            ReductionTree firstTree = new ReductionTree(firstProofSituation);
+            bool firstTruth = firstTree.IsAxiome();
+            Console.WriteLine(firstTree.ToString());
+            Console.WriteLine(firstTruth);
+
+            // |= (a ^ b) => (!a V !b)
+            var falseRightSide =
+                new Implication(
+                    new And(
+                        new Atomic("a"),
+                        new Atomic("b")),
+                    new Or(
+                        new Negate(
+                            new Atomic("a")),
+                        new Negate(
+                            new Atomic("b"))));
+            ReductionTree secondTree = new ReductionTree(new ProofSituation(falseRightSide));
+            bool secondTruth = secondTree.IsAxiome();
+            Console.WriteLine(secondTruth);
+
         }
 
     } 
